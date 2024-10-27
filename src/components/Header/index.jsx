a84ca4bcb2c3b09385fd0af4d8a1d4aad39d32d7
Anton Nakonechnyi <nakonechnyi.an@gmail.com>
2024-10-27 18:08:01 +0200

    add home page link

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,32 @@
-import { Heading, Divider } from "@chakra-ui/react";
+import { Heading, Divider, Link } from "@chakra-ui/react";
+import { NavLink, useLocation } from "react-router-dom";
 
 function Header() {
+  const location = useLocation();
+
   return (
     <header>
-      <Heading textAlign="center" my={3}>
+      <Heading textAlign="center" my={3} style={{ position: "relative" }}>
+        {location.pathname !== "/" && (
+          <Link
+            as={NavLink}
+            to="/"
+            color="blue.500"
+            style={{
+              fontSize: 14,
+              fontWeight: 400,
+              position: "absolute",
+              top: "50%",
+              left: "20px",
+              transform: "translateY(-50%)",
+              height: 40,
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            Home page
+          </Link>
+        )}
         Todo list
       </Heading>
       <Divider />
